4ba236f4f5a66f4de14e43ea54fbea9d0aedfd0c
L3oZX <155212554+L3oZX@users.noreply.github.com>
2026-07-29 20:08:28 -0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: ['chrome80', 'safari15', 'firefox80', 'edge80']
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+})
